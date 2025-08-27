5e29d58daf559cea85ab32653d1016dca07ccaf8
mo-lab <55297854+mo-lab@users.noreply.github.com>
2025-08-27 15:56:53 +0330

    readme

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/mo-lab/jobv/api/v2/internal/api/handlers"
+	mw "github.com/mo-lab/jobv/api/v2/internal/api/middlewares"
 )
 
 func main() {
@@ -13,8 +14,8 @@ func main() {
 	mux.HandleFunc("POST /otp/verify", handlers.VerifyOTPHandler)
 	mux.HandleFunc("GET /users", handlers.GetUsersHandler)
 	mux.HandleFunc("GET /users/search", handlers.SearchUsersHandler)
-
-	//http.HandleFunc("/users/search", jwtAuth(searchHandler))
+	// Protect the search endpoint with JWT authentication
+	mux.HandleFunc("GET /protected/users/search", mw.JwtAuth(handlers.SearchUsersHandler))
 
 	log.Println("Listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
--- internal/api/middlewares/jwt.go
+++ internal/api/middlewares/jwt.go
@@ -1,1 +1,34 @@
 package middlewares
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecretKey = []byte("your_secret_key_here")
+
+// JWT middleware
+func JwtAuth(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		auth := r.Header.Get("Authorization")
+		if !strings.HasPrefix(auth, "Bearer ") {
+			http.Error(w, "Missing or invalid Authorization header", http.StatusUnauthorized)
+			return
+		}
+		tokenString := strings.TrimPrefix(auth, "Bearer ")
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if token.Method != jwt.SigningMethodHS256 {
+				return nil, fmt.Errorf("unexpected signing method")
+			}
+			return jwtSecretKey, nil
+		})
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+		next(w, r)
+	}
+}
